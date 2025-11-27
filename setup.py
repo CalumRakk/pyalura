110864171701ca7f4a776aa3996069196671c39b
Leo <leocasti2@gmail.com>
2025-11-27 09:12:21 -0500

    feat: actualiza la versión del paquete a 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyalura",
-    version="0.1.0",
+    version="0.2.0",
     description="",
     author="Leo",
     author_email="leocasti@gmail.com",
